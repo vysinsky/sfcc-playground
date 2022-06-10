ee81213772d0127851e6cbfca1bcda2a1532cc77
Michal Vyšinský <michal@vysinsky.cz>
2022-06-10 13:10:24 +0200

    test: improve file system mocks

--- __tests__/server/analyzer/ControllersRegistry.test.js
+++ __tests__/server/analyzer/ControllersRegistry.test.js
@@ -1,206 +1,172 @@
+const { realpathSync } = require('fs');
+
 const controllersRegistry = require('../../../server/src/analyzer/ControllersRegistry');
 
-jest.mock('fs');
+function verifyControllersRegistryData(expected, actual) {
+  Object.entries(expected).forEach(([controllerName, expectedData]) => {
+    const actualData = actual[controllerName];
+
+    expect(actualData.cartridges).toEqual(expectedData.cartridges);
+
+    Object.entries(expectedData.filePaths).forEach(([key, value]) => {
+      expect(actualData.filePaths).toHaveProperty(key);
+      expect(actualData.filePaths[key]).toMatch(value);
+    });
+
+    expect(actualData.filePaths).toMatchObject(expectedData.filePaths);
+  });
+}
 
 describe('ControllersRegistry', () => {
-  const CARTRIDGE_A_CONTENT = [
-    'Home.js',
-    'Account.js',
-    'Tile.js',
-    '.eslintrc.json',
-    'small.js',
-  ];
-
-  const CARTRIDGE_B_CONTENT = [
-    'Home.js',
-    'Product.js',
-    'Tile.js',
-    '.eslintrc.json',
-    'small.js',
-  ];
-
   beforeEach(() => {
     controllersRegistry.controllers = {};
-    // Set up some mocked out file info before each test
-    require('fs').__setMockDirectoryContent(
-      'cartridges/cartridge_a/cartridge/controllers',
-      CARTRIDGE_A_CONTENT
-    );
-    require('fs').__setMockDirectoryContent(
-      'cartridges/cartridge_b/cartridge/controllers',
-      CARTRIDGE_B_CONTENT
-    );
   });
 
   test('registers all controllers from single cartridge', () => {
     global.playgroundConfig = {
-      cartridgesDir: 'cartridges',
+      cartridgesDir: realpathSync(`${__dirname}/../../../__mocks__/cartridges`),
     };
 
     controllersRegistry.registerAllControllers('cartridge_a');
 
-    expect(controllersRegistry.controllers).toMatchInlineSnapshot(`
-      Object {
-        "Account": Object {
-          "cartridges": Array [
-            "cartridge_a",
-          ],
-          "filePaths": Object {
-            "cartridge_a": "cartridges/cartridge_a/cartridge/controllers/Account.js",
+    verifyControllersRegistryData(
+      {
+        Account: {
+          cartridges: ['cartridge_a'],
+          filePaths: {
+            cartridge_a:
+              /__mocks__\/cartridges\/cartridge_a\/cartridge\/controllers\/Account\.js/,
           },
         },
-        "Home": Object {
-          "cartridges": Array [
-            "cartridge_a",
-          ],
-          "filePaths": Object {
-            "cartridge_a": "cartridges/cartridge_a/cartridge/controllers/Home.js",
+        Home: {
+          cartridges: ['cartridge_a'],
+          filePaths: {
+            cartridge_a:
+              /__mocks__\/cartridges\/cartridge_a\/cartridge\/controllers\/Home\.js/,
           },
         },
-        "Tile": Object {
-          "cartridges": Array [
-            "cartridge_a",
-          ],
-          "filePaths": Object {
-            "cartridge_a": "cartridges/cartridge_a/cartridge/controllers/Tile.js",
+        Tile: {
+          cartridges: ['cartridge_a'],
+          filePaths: {
+            cartridge_a:
+              /__mocks__\/cartridges\/cartridge_a\/cartridge\/controllers\/Tile\.js/,
           },
         },
-      }
-    `);
+      },
+      controllersRegistry.controllers
+    );
   });
 
   test('registers all controllers from multiple cartridges (A first)', () => {
     global.playgroundConfig = {
-      cartridgesDir: 'cartridges',
+      cartridgesDir: realpathSync(`${__dirname}/../../../__mocks__/cartridges`),
     };
 
     controllersRegistry.registerAllControllers('cartridge_a:cartridge_b');
 
-    expect(controllersRegistry.controllers).toMatchInlineSnapshot(`
-      Object {
-        "Account": Object {
-          "cartridges": Array [
-            "cartridge_a",
-          ],
-          "filePaths": Object {
-            "cartridge_a": "cartridges/cartridge_a/cartridge/controllers/Account.js",
+    verifyControllersRegistryData(
+      {
+        Account: {
+          cartridges: ['cartridge_a'],
+          filePaths: {
+            cartridge_a:
+              /__mocks__\/cartridges\/cartridge_a\/cartridge\/controllers\/Account\.js/,
           },
         },
-        "Home": Object {
-          "cartridges": Array [
-            "cartridge_a",
-            "cartridge_b",
-          ],
-          "filePaths": Object {
-            "cartridge_a": "cartridges/cartridge_a/cartridge/controllers/Home.js",
-            "cartridge_b": "cartridges/cartridge_b/cartridge/controllers/Home.js",
+        Home: {
+          cartridges: ['cartridge_a', 'cartridge_b'],
+          filePaths: {
+            cartridge_a:
+              /__mocks__\/cartridges\/cartridge_a\/cartridge\/controllers\/Home\.js/,
+            cartridge_b:
+              /__mocks__\/cartridges\/cartridge_b\/cartridge\/controllers\/Home\.js/,
           },
         },
-        "Product": Object {
-          "cartridges": Array [
-            "cartridge_b",
-          ],
-          "filePaths": Object {
-            "cartridge_b": "cartridges/cartridge_b/cartridge/controllers/Product.js",
+        Tile: {
+          cartridges: ['cartridge_a', 'cartridge_b'],
+          filePaths: {
+            cartridge_a:
+              /__mocks__\/cartridges\/cartridge_a\/cartridge\/controllers\/Tile\.js/,
+            cartridge_b:
+              /__mocks__\/cartridges\/cartridge_b\/cartridge\/controllers\/Tile\.js/,
           },
         },
-        "Tile": Object {
-          "cartridges": Array [
-            "cartridge_a",
-            "cartridge_b",
-          ],
-          "filePaths": Object {
-            "cartridge_a": "cartridges/cartridge_a/cartridge/controllers/Tile.js",
-            "cartridge_b": "cartridges/cartridge_b/cartridge/controllers/Tile.js",
-          },
-        },
-      }
-    `);
+      },
+      controllersRegistry.controllers
+    );
   });
 
   test('registers all controllers from multiple cartridges (B first)', () => {
     global.playgroundConfig = {
-      cartridgesDir: 'cartridges',
+      cartridgesDir: realpathSync(`${__dirname}/../../../__mocks__/cartridges`),
     };
 
     controllersRegistry.registerAllControllers('cartridge_b:cartridge_a');
 
-    expect(controllersRegistry.controllers).toMatchInlineSnapshot(`
-      Object {
-        "Account": Object {
-          "cartridges": Array [
-            "cartridge_a",
-          ],
-          "filePaths": Object {
-            "cartridge_a": "cartridges/cartridge_a/cartridge/controllers/Account.js",
+    verifyControllersRegistryData(
+      {
+        Account: {
+          cartridges: ['cartridge_a'],
+          filePaths: {
+            cartridge_a:
+              /__mocks__\/cartridges\/cartridge_a\/cartridge\/controllers\/Account\.js/,
           },
         },
-        "Home": Object {
-          "cartridges": Array [
-            "cartridge_b",
-            "cartridge_a",
-          ],
-          "filePaths": Object {
-            "cartridge_a": "cartridges/cartridge_a/cartridge/controllers/Home.js",
-            "cartridge_b": "cartridges/cartridge_b/cartridge/controllers/Home.js",
+        Home: {
+          cartridges: ['cartridge_b', 'cartridge_a'],
+          filePaths: {
+            cartridge_b:
+              /__mocks__\/cartridges\/cartridge_b\/cartridge\/controllers\/Home\.js/,
+            cartridge_a:
+              /__mocks__\/cartridges\/cartridge_a\/cartridge\/controllers\/Home\.js/,
           },
         },
-        "Product": Object {
-          "cartridges": Array [
-            "cartridge_b",
-          ],
-          "filePaths": Object {
-            "cartridge_b": "cartridges/cartridge_b/cartridge/controllers/Product.js",
+        Tile: {
+          cartridges: ['cartridge_b', 'cartridge_a'],
+          filePaths: {
+            cartridge_b:
+              /__mocks__\/cartridges\/cartridge_b\/cartridge\/controllers\/Tile\.js/,
+            cartridge_a:
+              /__mocks__\/cartridges\/cartridge_a\/cartridge\/controllers\/Tile\.js/,
           },
         },
-        "Tile": Object {
-          "cartridges": Array [
-            "cartridge_b",
-            "cartridge_a",
-          ],
-          "filePaths": Object {
-            "cartridge_a": "cartridges/cartridge_a/cartridge/controllers/Tile.js",
-            "cartridge_b": "cartridges/cartridge_b/cartridge/controllers/Tile.js",
-          },
-        },
-      }
-    `);
+      },
+      controllersRegistry.controllers
+    );
   });
 
   test('ignores non-existent cartridges', () => {
     global.playgroundConfig = {
-      cartridgesDir: 'cartridges',
+      cartridgesDir: realpathSync(`${__dirname}/../../../__mocks__/cartridges`),
     };
 
     controllersRegistry.registerAllControllers('cartridge_c:cartridge_a');
 
-    expect(controllersRegistry.controllers).toMatchInlineSnapshot(`
-      Object {
-        "Account": Object {
-          "cartridges": Array [
-            "cartridge_a",
-          ],
-          "filePaths": Object {
-            "cartridge_a": "cartridges/cartridge_a/cartridge/controllers/Account.js",
+    verifyControllersRegistryData(
+      {
+        Account: {
+          cartridges: ['cartridge_a'],
+          filePaths: {
+            cartridge_a:
+              /__mocks__\/cartridges\/cartridge_a\/cartridge\/controllers\/Account\.js/,
           },
         },
-        "Home": Object {
-          "cartridges": Array [
-            "cartridge_a",
-          ],
-          "filePaths": Object {
-            "cartridge_a": "cartridges/cartridge_a/cartridge/controllers/Home.js",
+        Home: {
+          cartridges: ['cartridge_a'],
+          filePaths: {
+            cartridge_a:
+              /__mocks__\/cartridges\/cartridge_a\/cartridge\/controllers\/Home\.js/,
           },
         },
-        "Tile": Object {
-          "cartridges": Array [
-            "cartridge_a",
-          ],
-          "filePaths": Object {
-            "cartridge_a": "cartridges/cartridge_a/cartridge/controllers/Tile.js",
+        Tile: {
+          cartridges: ['cartridge_a'],
+          filePaths: {
+            cartridge_a:
+              /__mocks__\/cartridges\/cartridge_a\/cartridge\/controllers\/Tile\.js/,
           },
         },
-      }
-    `);
+      },
+      controllersRegistry.controllers
+    );
   });
 });
