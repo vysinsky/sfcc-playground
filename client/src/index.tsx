b46d392352720332e01c606fa310309f52fbcd6e
Michal Vyšinský <michal@vysinsky.cz>
2022-06-14 15:56:36 +0200

    chore: remove web vitals reporting

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const root = ReactDOM.createRoot(
@@ -14,8 +13,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
